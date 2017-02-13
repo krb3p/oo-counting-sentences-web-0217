4b191f0aa2283143637d500ba1ea0ada302ce343
krb3p <ryankatherineburke@gmail.com>
2017-02-13 11:08:13 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    punctuation = "." || "!" || "?"
+    self.end_with? (punctuation)
   end
 
   def question?
+    question_mark = "?"
+    self.end_with? (question_mark)
 
   end
 
   def exclamation?
-
+    exclamation = "!"
+    self.end_with? (exclamation)
   end
 
+
   def count_sentences
+    x = self.split(/\.|\!|\?/).delete_if {|word| word.size < 2}
+    x.size
 
   end
 end
+
+# x = "one! , two. three?".split(/\.|\!|\?/)
